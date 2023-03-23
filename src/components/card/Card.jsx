36f40d0a9bbae455b8c92cea628b08304385592c
EmilceAide <emilceaide.t@gmail.com>
2023-03-23 02:03:02 -0500

    added: detail navigation

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
 import styles from './card.module.css'
 
-const Card = ({ name, status, species, gender, origin, image, onClose }) => {
+const Card = ({id, name, status, species, gender, origin, image, onClose }) => {
 
    return (
       <div className={styles.container}>
+         <button onClick={onClose} className={styles.close}>X</button>
+         <Link to={`/detail/${id}`}>
          <img src={image} alt='Avatar del personaje' className={styles.image} />
          <h2 className={styles.name}>{name}</h2>
          <h2 className={styles.origin}>{origin}</h2>
          <h2 className={styles.species}>{species}</h2>
          <h2 className={styles.gender}>{gender}</h2>
          <h2 className={styles.status}>{status}</h2>
-         <button onClick={onClose} className={styles.close}>X</button>
+         </Link>
       </div>
    );
 }
