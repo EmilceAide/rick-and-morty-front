7130f07addc59711adbc03ba15514855a2fb2fc0
EmilceAide <emilceaide.t@gmail.com>
2023-03-23 01:59:03 -0500

    home and about button was created

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "../searchBar/SearchBar.jsx";
 import styles from "./navBar.module.css";
 
 const NavBar = ({ onSearch, onGetRandom }) => {
   return (
     <div className={styles.container}>
+        <Link to="/home">
+            <button> Home </button>
+        </Link>
+        <Link to="/about" >
+            <button>About</button>
+        </Link>
       <SearchBar onSearch={onSearch} />
       <button
         onClick={() => onGetRandom(1, 826, onSearch)}
